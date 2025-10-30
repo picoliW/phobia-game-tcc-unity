b47649d5aefd48bff8707606e787126e781558e4
Luiz <leduardo3011@gmail.com>
2025-10-30 18:28:55 -0300

    feat: add nictophobia level 2

---
+++ Assets/Scripts/GoToNictophobiaLevel2.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GoToNictophobiaLevel2 : MonoBehaviour
+{
+    public GameObject avisoUI;
+    private bool avisoAtivo = false;
+
+    public void LoadNictophobiaLevel2(int opcao)
+    {
+        if (opcao == 1 || opcao == 2)
+        {
+            SceneManager.LoadScene("NictophobiaLevel2");
+        }
+        else if (opcao == 3 || opcao == 4)
+        {
+            if (!avisoAtivo)
+            {
+                avisoUI.SetActive(true);
+                avisoAtivo = true;
+            }
+            else
+            {
+                SceneManager.LoadScene("NictophobiaLevel2");
+            }
+        }
+        else if (opcao == 5)
+        {
+            StartCoroutine(MostrarAvisoEFechar());
+        }
+    }
+
+    private IEnumerator MostrarAvisoEFechar()
+    {
+        avisoUI.SetActive(true); 
+        yield return new WaitForSeconds(5f); 
+        Application.Quit();
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; 
+        #endif
+    }
+}
